6b527a580f7a32e45d3db4af2cf7fa342926ce4e
Aaron <35376325+aaronnsmh@users.noreply.github.com>
2018-03-24 15:27:02 +0000

    Create app.js

---
+++ app.js
@@ -0,0 +1,50 @@
+// Go-Karting Xtreme Discord Bot Code
+// Version: 0.9 PRE-ALPHA / PRE-REALEASE
+// Discord Code: GR9qVVd
+
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const db = require('quick.db');
+const prefix = '=';
+
+client.on('ready', () => {
+    console.log(`Logged in as ${client.user.tag}! There are no apparent major bugs.`);
+    client.user.setActivity('over Go-Karting Xtreme | =help', { type: 'WATCHING' });
+    client.user.setStatus("online");
+});
+
+
+client.on('message', message => {    
+    
+    let sender = message.author;
+
+    if (sender.bot) return;
+    if (message.channel.type === 'dm') {
+        message.channel.send("**Unfortunately we can only read things our guild (server). Please join our server (code: GR9qVVd) to use the bot! **")
+        return;
+    }
+
+        let msg = message.content.toLowerCase();
+        let args = message.content.slice(prefix.length).trim().split(" ");
+        let cmd = args.shift().toLowerCase();
+
+        if (!message.content.startsWith(prefix)) return;
+
+        try {
+
+            let commandFile = require(`./commands/${cmd}.js`);
+            commandFile.run(Discord, client, message, args);
+
+        } catch (e) {
+
+            console.log(e);
+
+        } finally {
+
+            console.log(`${message.author.username} ran the command: ${cmd} sucessfully!`);
+
+        }
+    })
+
+
+client.login(process.env.TOKEN);
